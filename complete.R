68dd5b4792744486fc1540d01d098742ef4db805
agardmar000 <mark.d.agard@gmail.com>
2020-06-30 16:58:34 -0400

    function returns a dataframe instead of whatever I had it doing at first. cleaner and probably more useful for corr()

--- complete.R
+++ complete.R
@@ -1,13 +1,16 @@
 complete <- function(directory = "specdata", id = 1:332) {
+  df <- NULL
   for(i in id) {
-    files <- list.files(path="specdata", pattern="*.csv", full.names=TRUE, recursive=FALSE)
+    files <- list.files(path="specdata", pattern="*.csv", full.names=TRUE)
     x <- read.csv(files[i])
     x1 <- x[,1]
     x2 <- x[,2]
     x3 <- x[,3]
     x4 <- x[,4]
     good <- complete.cases(x1, x2, x3, x4)
-    sum <- sum(good)
-    print(sum)
+    nobs <- sum(good)
+    df <- rbind(df, data.frame(i, nobs))
+    
   }
+  
 }
